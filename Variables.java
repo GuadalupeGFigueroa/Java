3157b639f3fdec2942cc7440a3c49b8b0eec345f
GuadalupeGFigueroa <guadalupe.g.figueroa@gmail.com>
2024-09-20 13:06:00 +0200

    Ejercicio 1

---
+++ Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+        int stock = 50;
+        String name = "Tablones de Roble";
+
+        System.out.println(stock); 
+        System.out.println(name);
+        System.out.println("Quedan en el almacen: " + stock + " unidades."); 
+
+
+    }
+}
