a9555fb0eb6f28357128b6116660875142c6e6ae
GuadalupeGFigueroa <guadalupe.g.figueroa@gmail.com>
2024-09-20 15:15:38 +0200

    Ejercicios completos variables

--- Variables.java
+++ Variables.java
@@ -3,12 +3,28 @@ public class Variables {
     public static void main(String[] args) {
 
         int stock = 50;
+        int sold = 10; 
         String name = "Tablones de Roble";
 
         System.out.println(stock); 
         System.out.println(name);
-        System.out.println("Quedan en el almacen: " + stock + " unidades."); 
+        System.out.println(sold);
+        System.out.println("Quedan en el almacen: " + stock + " unidades. " +  "Se han vendido "+ sold + " unidades."); 
+       
+        //Intercambiar usando operaciones aritméticas:
+        sold = sold + stock;
+        stock = sold - stock;
+        sold = sold - stock;
 
+        System.out.println("Ahora el valor de la variable stock es de: " + stock); 
+        System.out.println("Ahora el valor de la variable sold es " + sold);
+
+        //Intercambiar usando XOR bit a bit: Intercambia dos variables enteras
+        sold = sold ^ stock;
+        stock = sold ^ stock;
+        sold = sold ^stock;
+        System.out.println("después del cambio el es stock es de: "+ stock); 
+        System.out.println("después del cambio se han vendido " + sold);
 
     }
 }
