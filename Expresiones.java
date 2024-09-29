6db4d9a5f7f31bd07ecc4024042395dadef495bb
GuadalupeGFigueroa <guadalupe.g.figueroa@gmail.com>
2024-09-30 01:33:30 +0200

    Elaboración índice de ejercicios

--- Expresiones.java
+++ Expresiones.java
@@ -1,6 +1,23 @@
 import java.util.Scanner;
+import java.lang.Math;
+
 
 public class Expresiones {
     public static void main(String[] args) {
+        //Área de un círculo = π r² 
         
-}
+        double radio;
+        double area;
+        Scanner scanner = new Scanner(System.in);
+            
+        System.out.println("Introduce el radio del círculo: ");
+        radio = scanner.nextDouble();    
+        
+        area = Math.PI * Math.pow(radio, 2);
+        
+        System.out.println("El área de un círculo con radio igual a " + radio + " es: "+ area );
+    
+    scanner.close();
+    }
+}        
+
---
+++ GUI.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class Gui {
+    public static void main(String[] args) {
+        String nombre = JOptionPane.showInputDialog("¿Cómo te llamas?");
+        JOptionPane.showMessage("¡Hola s" + nombre + "!");
+
+
+
+    }
+   
+}   
